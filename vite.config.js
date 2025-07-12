b63d43ff2cc23e449e12a4c979b7ca154e516b81
Wisss <wisnuput12@gmail.com>
2025-07-12 23:07:11 +0700

    Update: Vite

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      '/api': {
-        target: `${import.meta.env.VITE_API_PATH}`,
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, '/api'),
-      },
-    },
-    '/image-proxy': {
-        target: `${import.meta.env.VITE_API_PATH_IMAGE}`,
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/image-proxy/, ''),
+export default defineConfig(({ mode }) => {
+  // Load env variables from .env files
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      proxy: {
+        // Proxy untuk API
+        '/api': {
+          target: env.VITE_API_PATH,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '/api'),
+        },
+        // Proxy untuk gambar
+        '/image-proxy': {
+          target: env.VITE_API_PATH_IMAGE,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/image-proxy/, ''),
+        }
       }
-  }
-})
+    }
+  };
+});
