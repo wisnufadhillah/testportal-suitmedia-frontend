07e57bc12e02c24d0f990f0a21b2e93d4e2dd46f
Wisss <wisnuput12@gmail.com>
2025-07-19 00:09:21 +0700

    Fix: vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,28 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-})
+export default defineConfig(({ mode }) => {
+  // Load env variables from .env files
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      proxy: {
+        // Proxy untuk API
+        '/api': {
+          target: env.VITE_API_PATH,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '/api'),
+        },
+        // Proxy untuk gambar
+        '/image-proxy': {
+          target: env.VITE_API_PATH_IMAGE,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/image-proxy/, ''),
+        }
+      }
+    }
+  };
+});
